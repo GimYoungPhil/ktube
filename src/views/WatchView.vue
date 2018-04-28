6d12fc7133dd9f21ecf1ec392dec022b9a6826cb
GimYoungPhil <youngphil.gim@gmail.com>
2018-04-28 22:41:31 +0900

    fixed WatchView

--- src/views/WatchView.vue
+++ src/views/WatchView.vue
@@ -9,6 +9,15 @@
 <script>
 export default {
   name: 'WatchView',
+
+  mounted: function () {
+    const { videoKey } = this.$route.params
+    const { type, idol } = this.$route.query
+
+    console.log(videoKey)
+    console.log(type)
+    console.log(idol)
+  },
 }
 </script>
 
