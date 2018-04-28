d1b2997c51590702b4e44227f1928fffb45cad11
GimYoungPhil <youngphil.gim@gmail.com>
2018-04-28 22:40:14 +0900

    fixed router

--- src/router.js
+++ src/router.js
@@ -1,20 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import VideoView from '@/views/VideoView.vue'
-import FanCamView from '@/views/FanCamView.vue'
-import KaraokeView from '@/views/KaraokeView.vue'
+import TheVideo from '@/views/TheVideo.vue'
+import TheFanCam from '@/views/TheFanCam.vue'
+import TheKaraoke from '@/views/TheKaraoke.vue'
 import IdolView from '@/views/IdolView.vue'
-// import WatchView from '@/views/WatchView.vue'
+import WatchView from '@/views/WatchView.vue'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [
-    { path: '/',        name: 'video',   component: VideoView },
-    { path: '/fanCam',  name: 'fanCam',  component: FanCamView },
-    { path: '/karaoke', name: 'karaoke', component: KaraokeView },
-    { path: '/idols',    name: 'idol',   component: IdolView },
-    // { path: '/watch/:uid', component: WatchView },
+    { path: '/',                name: 'video',   component: TheVideo },
+    { path: '/fancam',          name: 'fancam',  component: TheFanCam },
+    { path: '/karaoke',         name: 'karaoke', component: TheKaraoke },
+    { path: '/idol:idolKey',    name: 'idol',    component: IdolView },
+    { path: '/watch/:videoKey', name: 'watch',   component: WatchView },
   ]
 })
 
