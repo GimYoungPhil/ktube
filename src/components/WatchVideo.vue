5987a1e08216476777b193c96a96f41aa993efe4
GimYoungPhil <youngphil.gim@gmail.com>
2018-04-30 23:33:47 +0900

    add WatchVideo

---
+++ src/components/WatchVideo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Watch Video {{ videoKey }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WatchVideo',
+  props: {
+    videoKey: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
